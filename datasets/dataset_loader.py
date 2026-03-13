6b55e3ebc755ace76789e9a000e2c5b4df4f3b4f
Rashi Raj <160142642+rasshhe@users.noreply.github.com>
2026-03-13 15:14:23 +0530

    Update dataset_loader.py

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -1,1 +1,29 @@
+from roboflow import Roboflow
 
+
+def load_traffic_dataset(api_key):
+    """
+    Load the UA-DETRAC traffic dataset
+    """
+
+    rf = Roboflow(api_key=api_key)
+
+    project = rf.workspace("rjacaac1").project("ua-detrac-dataset-10k")
+
+    dataset = project.version(1).download("yolov8")
+
+    return dataset
+
+
+def load_emergency_dataset(api_key):
+    """
+    Load emergency vehicle dataset
+    """
+
+    rf = Roboflow(api_key=api_key)
+
+    project = rf.workspace("jam-hyzhb").project("object-detection-xdvt2")
+
+    dataset = project.version(1).download("yolov8")
+
+    return dataset
